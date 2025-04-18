412b52412fce8ea10c899096535a7aeb7b3623e8
komaruks <komaruks@mail.ru>
2025-04-18 11:00:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
+// Проверяем тему при загрузке страницы
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-theme');
+}
+
+// При клике переключаем тему и сохраняем в localStorage
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark-theme');
+
+  if (body.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
